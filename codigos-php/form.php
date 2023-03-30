b76f11bc97e851273be3d30a507503a55c9545f1
Edmarcos de araujo <edmarcos1993@hotmail.com>
2023-03-29 22:45:41 -0300

    form com get e post

---
+++ codigos-php/form.php
@@ -0,0 +1,11 @@
+<?php
+$nome =$_GET["nome"];
+$email =$_GET["email"];
+$idade =$_GET["idade"];
+
+echo "bem vindo: $nome";
+echo "<br>meu email e : $email";
+echo "<br>minha idade  e : $idade";
+
+
+?>
---
+++ form-post.php
@@ -0,0 +1,11 @@
+<?php
+$nome =$_POST["nome"];
+$email =$_POST["email"];
+$idade =$_POST["idade"];
+
+echo "bem vindo: $nome";
+echo "<br>meu email e : $email";
+echo "<br>minha idade  e : $idade";
+
+
+?>
